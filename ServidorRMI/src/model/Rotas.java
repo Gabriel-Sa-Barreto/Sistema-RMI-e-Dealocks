c426763119e998db68ed25eb1cec62ccfca2addf
Samuel Vitorio Lima <34277130+SamuelLima0610@users.noreply.github.com>
2019-09-05 10:30:25 -0300

    servidorRMI
    Servidor RMI

---
+++ ServidorRMI/src/model/Rotas.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ * Classe responsavel por guardar a rota
+ */
+
+/**
+ *
+ * @author lsjsa
+ */
+public class Rotas {
+    
+    /**
+     * Atributo que armazena o local onde o trajeto começa
+     */
+    private String origem;
+    /**
+     * Atributo que armazena o destino do trajeto
+     */
+    private String destino;
+    /**
+     * Atributo que armazena a quantidade de passagens sobrando
+     */
+    private int quantidade;
+    /**
+     * Atributo que armazena a companhia responasel por esse trajeto
+     */
+    private String companhia;
+
+    /**
+     * Construtor da model Rotas
+     * @param origem
+     * @param destino
+     * @param quantidade
+     * @param companhia 
+     */
+    public Rotas(String origem, String destino, int quantidade, String companhia) {
+        this.origem = origem;
+        this.destino = destino;
+        this.quantidade = quantidade;
+        this.companhia = companhia;
+    }
+
+    /**
+     * Metodo get do atributo origem
+     * @return 
+     */
+    public String getOrigem() {
+        return origem;
+    }
+
+    /**
+     * Metodo get do atributo destino
+     * @return 
+     */
+    public String getDestino() {
+        return destino;
+    }
+
+    /**
+     * Metodo get do atributo quantidade
+     * @return 
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * Metodo get do atributo companhia
+     * @return 
+     */
+    public String getCompanhia() {
+        return companhia;
+    }
+
+    /**
+     * Metodo set do atributo quantidade
+     * @param quantidade 
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+}
---
+++ ServidorRMI/src/servidorRMI/ClienteRMI.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servidorRMI;
+import java.io.IOException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Rotas;
+/**
+ *
+ * @author lsjsa
+ */
+public class ClienteRMI {
+    
+    private static Servico servico = null;
+    private static List<Rotas> trajetos;
+    
+    public ClienteRMI() {
+        try{
+            //string que deve conter o endereco onde o serviço está sendo
+            //disponibilizado e o nome do serviço
+            String localizacao = "//localhost/servico";
+            servico = (Servico) Naming.lookup(localizacao);
+        } catch(Exception ex){
+            System.out.println("Erro" + ex.getMessage());
+        }
+    }
+    
+     public static void main(String[] args) throws IOException {
+         ClienteRMI cliente = new ClienteRMI();
+         trajetos = servico.lerRotas();
+         trajetos.forEach(u -> System.out.println(u.getOrigem() + "-" + u.getDestino() + "-" + u.getCompanhia()));
+         System.out.println(servico.consultarQuantidadeDePassagens("Terezina","Natal"));
+         servico.mudarQuantidadeDePassagens("Terezina","Natal",15);
+         System.out.println(servico.consultarQuantidadeDePassagens("Terezina","Natal"));
+     }
+    
+}
