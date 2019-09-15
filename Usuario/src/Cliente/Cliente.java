36c58a570880ed7b16202be673ea67b293ea297c
Gabriel-Sa-Barreto <bielbarretoalves@gmail.com>
2019-09-15 11:48:36 -0300

    Comunicação entre servidor e aplicação do cliente

---
+++ Usuario/src/Cliente/Cliente.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cliente;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ *
+ * @author Gabriel Sá e Samuel Vitorio
+ */
+public class Cliente {
+     /**Atributo responsável por guardar o ip da máquina que o cliente irá se conectar.
+    */
+    private String host;
+    
+    /**Atributo responsável por guardar a porta do servidor para conexão.
+     */
+    private int porta;
+    
+    /**Atributo responsável por guardar o socket do cliente.
+     */
+    private Socket cliente;
+    
+    /**Atributo responsável por guardar o objeto que será executado em uma thread para recebimento dos pacotes do servidor.
+     */
+    private Recebedor receive;
+    
+    /**Construtor da classe Cliente que é responsável por criar um novo cliente no sistema e conectá-lo ao servidor  
+    * @param host String -Ip da máquina ao qual o cliente irá se conectar
+    * @param porta int    - Porta do servidor para conexão
+    * @author Samuel Vitorio Lima e Gabriel Sá Barreto 
+    * @throws java.io.IOException 
+    */
+    public Cliente (String host, int porta) throws IOException {
+        this.host = host;
+        this.porta = porta;
+        this.conexao();
+    }
+    
+    /**Método para conectar o cliente ao servidor, este método é ao clicar um novo objeto do tipo Cliente.
+    *  @author Samuel Vitorio Lima e Gabriel Sá Barreto 
+    */
+    private void conexao() throws IOException{
+        cliente = new Socket(this.host, this.porta);
+        System.out.println("O cliente se conectou ao servidor!");
+    }
+    
+    
+    /**
+     * Método responsável pela criação e execução da thread responsável por receber pacotes vindos do servidor 
+     * para o objeto cliente.
+     * @author Samuel Vitorio Lima e Gabriel Sá Barreto
+     * @throws UnknownHostException
+     * @throws IOException 
+     */
+    public void executa() throws UnknownHostException, IOException {
+        // thread para receber mensagens do servidor
+        receive = new Recebedor(cliente.getInputStream(), this.cliente);
+        new Thread(receive).start();
+    }
+
+    /**
+     * Método que retorna o objeto socket que está vinculado ao cliente
+     * @author Samuel Vitorio Lima e Gabriel Sá Barreto
+     * @return 
+     */
+    public Socket getCliente() {
+        return cliente;
+    }
+
+    /**
+     * Método para configurar o objeto socket que está vinculado ao cliente.
+     * @author Samuel Vitorio Lima e Gabriel Sá Barreto
+     * @param cliente 
+     */
+    public void setCliente(Socket cliente) {
+        this.cliente = cliente;
+    }
+}
---
+++ Usuario/src/Cliente/Recebedor.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cliente;
+
+import ControllerPacote.ControllerPacote;
+import ControllerRede.ControllerRede;
+import ControllerRotas.ControllerRotas;
+import java.io.*;
+import java.net.Socket;
+
+/**
+ *
+ * @author Gabriel Sá e Samuel Vitorio 
+ */
+public class Recebedor implements Runnable{
+    /**
+     * Atributo que receberá o Inputstream do servidor conectado.
+     */
+    private InputStream servidor;
+    
+    /**
+     * Atributo que configura se as atividades da serão executadas.
+     */
+    private boolean start;
+    
+    /**Atributo responsável por guardar uma cópia do socket do cliente.
+     */
+    private Socket cliente;
+    
+    /**
+     * Atributo que guarda o pacote padrão de fechamento de conexão com o servidor.
+     */
+    private  String fechar = "3" + ";" + "Vazio";
+    
+    /**Construtor da classe Recebedor que inicia todos os atributos. 
+     * @author Samuel Vitorio Lima e Gabriel Sá Barreto 
+     * @param servidor - Atributo responsável por receber o objeto InputStream do servidor.
+     * @param cliente  - Atributo responsável por receber o socket do cliente.
+     */
+    public Recebedor(InputStream servidor, Socket cliente) {
+        this.servidor = servidor;
+        this.cliente = cliente;
+        this.start    = true;
+    }
+   
+    
+    @Override
+    public void run() {
+        String pacote = "";
+        int acao = 0;
+        DataInputStream entrada = new DataInputStream(this.servidor);
+        while(start){
+            //recebe mensagem do servidor
+            try {
+                //recebimento do pacote de dados do usuário
+                pacote = entrada.readUTF();
+                String dados[] = pacote.split(";");
+                acao = ControllerPacote.strToInt(dados[0],0);
+                switch(acao) {
+                    case 1:
+                        //recebimento das rotas calculadas pelo servidor.
+                        //apartir da origem e destino enviadas pelo usuário.
+                        //formato do pacote:
+                        //opcode;rota;sinalDeFinalizado
+                        int finalizadoRotas = ControllerPacote.strToInt(dados[2], 2);
+                        if(finalizadoRotas == 1){
+                            //terá um próximo pacote de rota.
+                            ControllerRotas.setHasNewRoutes(true);
+                            //adiciona uma nova rota
+                            ControllerRotas.addNovaRota(dados[1]);
+                        }else if(finalizadoRotas == 0){
+                            //não terá um próximo pacote de rota.
+                            ControllerRotas.setHasNewRoutes(false);
+                            //envia pacote para encerrar conexão com o servidor.
+                            ControllerRede.enviarDado(cliente, this.fechar);
+                            //encerra o loop da thread
+                            start = false;
+                        }
+                        break;
+                    case 2:
+                        //recebimento de confirmação/erro do trajeto escolhido.
+                        if(dados[1].equals("Compra-Realizada")){
+                            ControllerRotas.setHasNewTrechoFailed(false);
+                            //envia pacote para encerrar conexão com o servidor.
+                            ControllerRede.enviarDado(cliente, this.fechar);
+                            //encerra o loop da thread
+                            start = false;
+                        }else{
+                            //recebe trajetos que deram erro.
+                            //formato do pacote:
+                            //opcode;trechoIndisponível;sinalDefinalizado
+                            int finalizadoTrechos = ControllerPacote.strToInt(dados[1], 2);
+                            if(finalizadoTrechos == 1){
+                                //mais trechos falhos serão enviados. 
+                                ControllerRotas.setHasNewTrechoFailed(true);
+                                //formato do dados:
+                                //saída;destino;empresa
+                                String trecho = dados[2] + ";" + dados[3] + ";" + dados[4];
+                                ControllerRotas.addTrechoFalho(trecho);
+                            }else if(finalizadoTrechos == 0){
+                               //acabou a transmissão de trechos falhos.
+                               ControllerRotas.setHasNewTrechoFailed(false);
+                               //envia pacote para encerrar conexão com o servidor.
+                               ControllerRede.enviarDado(cliente, this.fechar);
+                               //encerra o loop da thread
+                               start = false;
+                            }
+                        }
+                        break;
+                    default:
+                        System.out.println("Erro no campo de ação do cliente!!");
+                        break;
+                }
+            } catch (IOException ex) {
+                System.out.println("Erro de recebimento de dados: " + ex.getMessage());
+            }
+        }
+    }
+}
